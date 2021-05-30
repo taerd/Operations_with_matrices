21e61a180b2ca3453ed893c95a63f44628a356c5
Артур Имамутдинов <ffifalafiesta@gmail.com>
2021-05-30 23:52:39 +0300

    Возможность остановки сервера по STOP

--- Server/src/main.kt
+++ Server/src/main.kt
@@ -1,8 +1,20 @@
+import java.util.*
+
 fun main(){
+
     val s = Server()
-    s.start()
 
-    val db = s.DBHelper("matrix.sql")
-    db.createDatabase("matrix.sql")
+    val db = DBHelper("matrix")
+    db.createDatabase("matrix")
+    db.fillTableFromCsv("data.csv")
 
+    s.start("data",db)
+
+    //Сервер остановится при вводе "STOP" в его консоль
+    var cmd: String
+    val sc= Scanner(System.`in`)
+    do{
+        cmd=sc.nextLine()
+    }while (cmd!="STOP")
+    s.stop()
 }
